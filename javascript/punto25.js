78b6c434f34d6f5a82064f420a2a287f6ee40605
25Alan <romeroalan71@gmail.com>
2022-06-04 14:12:19 -0300

    organizacion de las carpetas y demás archivos

--- javascript/punto25.js
+++
@@ -1,9 +0,0 @@
-const num1 = document.querySelector('#num1');
-const num2 = document.querySelector('#num2');
-const cap = document.querySelector('button');
-
-function show(){
-    alert("Valor input 1 =" + num1.value+" || " + "Valor input 2 =" + num2.value);
-}
-
-cap.onclick = show;
